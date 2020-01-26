fb561d71727996e94515689551effaee2e6a37bc
hervind phil <hervindruid@gmail.com>
2020-01-26 21:30:48 +0700

    Update insert_test.py

--- insert_test.py
+++ insert_test.py
@@ -1,5 +1,9 @@
-from main import db, User
+from main import db, User, Submission
 
-u = User(username="karjo", password = "karjo")
+u = User(username="karje", password = "karje")
 db.session.add(u)
+db.session.commit()
+
+s = Submission(user_id=1 , score=0.5, submission_type = 'public')
+db.session.add(s)
 db.session.commit()
