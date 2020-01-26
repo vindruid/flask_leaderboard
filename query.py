d2dd606749ad598f275689e7bad72d457783afa4
hervind phil <hervindruid@gmail.com>
2020-01-26 21:28:20 +0700

    Update query.py

--- query.py
+++ query.py
@@ -1,5 +1,11 @@
-from main import Submission 
+from main import Submission, User
 
 subs = Submission.query.all()
+print("SUBMISSION: ")
 for sub in subs:
     print(sub.user_id, sub.score)
+
+users = User.query.all()
+print("USERS: ")
+for user in users:
+    print(user.username, user.password)
